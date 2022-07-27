0fc2343d6774fe2e6ba68aff6a5f415993c3ebdf
naro-Kim <gracias9022@gmail.com>
2022-07-27 20:21:03 +0900

    [Add] 가상환경 설정
    가상환경 설정 연습

--- Frontend/eatlion/src/API/LoginAPI.jsx
+++ Frontend/eatlion/src/API/LoginAPI.jsx
@@ -2,19 +2,20 @@ import axios from "axios";
 
 // userid, password 입력 받으면 token 값 return
 // error 시, token = '', 성공 시, token = 'Token 값'
-const LogInAPI = async (userid,password) => {
-  let token = ''
-  await axios.post("http://localhost:8000/user/login/", {
-    userid: userid,
-    password: password,
-  })
-  .then((response) => {
-    token = response.data.Token;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+const LogInAPI = async (userid, password) => {
+  let token = "";
+  await axios
+    .post("https://postman-echo.com/POST", {
+      userid: userid,
+      password: password,
+    })
+    .then((response) => {
+      token = response.data.Token;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   return token;
-}
+};
 
 export default LogInAPI;
