0be03e07d5cb2fcce5efb0d43e4243c4c7749ce4
shinddong <shinkent@mail.hongik.ac.kr>
2022-08-05 17:08:31 +0900

    [Update]:Map.jsx

--- Frontend/eatlion/src/Components/Map.jsx
+++ Frontend/eatlion/src/Components/Map.jsx
@@ -9,7 +9,7 @@ const Map = () => {
     useEffect(() => {
         const container = document.getElementById('myMap');
 		const options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			center: new kakao.maps.LatLng(37.55036,126.92544),
 			level: 3
 		};
         const map = new kakao.maps.Map(container, options);
