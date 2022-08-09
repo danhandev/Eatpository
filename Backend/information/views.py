8ff267debd410bcc13a184bd85dfd4acda33ddd1
danhan <danhandev@gmail.com>
2022-08-09 21:51:28 +0900

    DOCS: Write code description

--- Backend/information/views.py
+++ Backend/information/views.py
@@ -1,34 +1,21 @@
-from genericpath import exists
-from django.shortcuts import render
-
 from information.serializers import InformationSerializer
 from accounts.models import Users
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-# import json
-# Create your views here.
 
 
 @api_view(['GET'])
 def get_editors(request):
     if request.method == "GET":
         try:
+            # Users table에서 editor 정보만 선택한다.
             editors = Users.objects.filter(role=True)
+            # list를 반환하기 위해 many=True 조건을 단다.
             serialized_editors = InformationSerializer(editors, many=True)
 
             return Response({"editors": serialized_editors.data})
         except:
             return Response({"message": "Cannot find categories"})
 
-        # print(categories)
-        # obj_num = categories.count()
-
-        # for x in range(obj_num):
-        #     category = InformationSerializer(categories[x])
-        #     categories_list.append(category.data)
-
-        # if editors is :
-
-        # else:
-        #     return Response({"message": "editors does not exist"})
+    else:
+        return Response({"message": "Invalid method"})
